a68be1fa54b0ff18f6e46172287520389fbc621d
ann <a.lopez@naturalopticsgroup.com>
2019-05-04 20:49:37 +0200

    shi

--- PHP/script.js
+++ PHP/script.js
@@ -8,12 +8,13 @@ $(document).ready(function(){
       var tipo=$('#tipo').val();
       var pass =$('#pass').val();
       var pass2=$('#pass2').val();
+      var opcion= $("#opcion-form").val();
 
 
       $.ajax({
         type: 'POST',
         url: 'registro_final.php',
-        data: { nombre: nombre, apellido: apellido, apellido2: apellido2, correo: correo, tipo: tipo, pass: pass, pass2: pass2 },
+        data: { nombre: nombre, apellido: apellido, apellido2: apellido2, correo: correo, tipo: tipo, pass: pass, pass2: pass2, opcion: opcion },
         success: function(data) {
             console.log(data);
         }
