01cf9367d8131de7c1981725a8b59ce6364050cb
ann <a.lopez@naturalopticsgroup.com>
2019-05-16 20:09:04 +0200

    u

--- proyecto_mvc/js/mediciones.js
+++ proyecto_mvc/js/mediciones.js
@@ -52,16 +52,64 @@ $( document ).ready(function() {
     $("#Select-id-usuario").change(function(){
         var id_usuario = this.value; //valor option del select
         if(id_usuario=="Todos"){
-            // $("tbody tr").show();
-            $("tbody tr").css("display","table-column");
+            $("tbody tr").show();
         }
         else{
-            //  $("tbody tr:not(#"+id_usuario+")").hide();
-            $("tbody tr").css("display","table-column");
-            $("tbody tr:not(#"+id_usuario+")").css("display","none");
+            $("tbody tr").show();
+            $("tbody tr:not(#"+id_usuario+")").hide();
         }
        
 
     });
 
+
+    function verPaciente(id_usuario){
+        $.ajax({
+            type: "POST",
+            url: 'control/vista.php',
+            data: { 
+                opcion: "mostrar_graficas",
+                id_usuario: id_usuario
+            }
+            
+            })        
+            .done(function( data, textStatus, jqXHR ) {
+                
+                if ( console && console.log ) {
+                    console.log( "ajax done" );
+                }
+                var datos = $.parseJSON(data);
+                var listado_pacientes = datos;
+                
+                //console.log(datos);
+                //RELLENAMOS TABLA
+                var filas_pacientes='';
+                datos.forEach(function(element) {
+                    filas_pacientes+= '<tr id='+element.id_user+'><td>'+element.id_user+'</td><td>'+element.nombre+'</td><td>'+element.apellido1+'</td><td>'+element.apellido2+'</td><td>'+element.correo+'</td><td>'+element.pass+'</td><td><button type="button" class="btn mt-1 rojo" value="verPaciente" onclick="verPaciente(\'' + element.id_user + '\')"><span class="ti-eye"></span></button><button type="button" class="btn azul" value="editarPaciente" onclick="editarPaciente(\'' + element.id_user + '\')"><span class="ti-pencil-alt"></span></button></td></tr>';
+
+                });
+                $('#pacientes-table tbody').html(filas_pacientes);
+
+                //RELLENAMOS SELECT
+                $("#Select-id-usuario").html("");
+                $('#Select-id-usuario').append($('<option>', { 
+                        value: "Todos",
+                        text :  "Todos"
+                }));
+                listado_pacientes.forEach(function(element) {
+                    //console.log(element);
+                    $('#Select-id-usuario').append($('<option>', { 
+                        value: element.id_user,
+                        text : element.id_user
+                    }));
+                });
+
+
+            })
+            .fail(function( jqXHR, textStatus, errorThrown ) {
+                if ( console && console.log ) {
+                    console.log( "La solicitud de acceso ha fallado: " +  textStatus);
+                }
+            });
+    }
 });
