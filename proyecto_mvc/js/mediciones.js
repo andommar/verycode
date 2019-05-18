470d3b0773355923c03d5a710d84bb5cc256e0e4
ann <a.lopez@naturalopticsgroup.com>
2019-05-18 17:16:14 +0200

    alga es alga

--- proyecto_mvc/js/mediciones.js
+++ proyecto_mvc/js/mediciones.js
@@ -23,7 +23,7 @@ var listado_pacientes="";
     //RELLENAMOS TABLA
     var filas_pacientes='';
     listado_pacientes.forEach(function(element) {
-        filas_pacientes+= '<tr id='+element.id_user+'><td>'+element.id_user+'</td><td>'+element.nombre+'</td><td>'+element.apellido1+'</td><td>'+element.apellido2+'</td><td>'+element.correo+'</td><td>'+element.pass+'</td><td><button type="button" class="btn mt-1 rojo" value="verPaciente" onclick="verPaciente(\'' + element.id_user + '\')"><span class="ti-eye"></span></button><button type="button" class="btn azul" value="editarPaciente" onclick="editarPaciente(\'' + element.id_user + '\')"><span class="ti-pencil-alt"></span></button></td></tr>';
+        filas_pacientes+= '<tr id='+element.id_user+'><td>'+element.id_user+'</td><td>'+element.nombre+'</td><td>'+element.apellido1+'</td><td>'+element.apellido2+'</td><td>'+element.correo+'</td><td>'+element.pass+'</td><td><button type="button" class="btn mt-1 rojo" value="verPaciente" onClick="verPaciente(' + element.id_user + ')"><span class="ti-eye"></span></button><button type="button" class="btn azul" value="editarPaciente" onClick="editarPaciente('+ element.id_user + ')"><span class="ti-pencil-alt"></span></button></td></tr>';
 
     });
     $('#pacientes-table tbody').html(filas_pacientes);
