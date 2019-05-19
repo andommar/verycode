f69c9e85dbfabf6b2183aa4641fd2594b9d29c93
ann <a.lopez@naturalopticsgroup.com>
2019-05-19 16:22:17 +0200

    fckng git

--- proyecto_mvc/display-grafica.php
+++ proyecto_mvc/display-grafica.php
@@ -185,7 +185,7 @@
                 </div> <!-- Fin columna derecha-->
             </div> <!-- ROW -->
 
-            <input id="usuario" type="hidden" value="17"> <!--<?php /*echo $_SESSION["id_usuario"]*/?>-->
+            <input id="usuario" type="hidden" value="<?php echo $_GET["id_user"]?>"> 
         </div><!-- CONTAINER FLUID-->
 
         <!-- SCRIPTS -->
